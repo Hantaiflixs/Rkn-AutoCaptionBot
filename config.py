2488d38d9c4ace6acfab04011e77515d1a76071b
Hantaiflixs <souravbosu017@gmail.com>
2026-03-12 16:06:11 +0600

    Restore copyright and contact information in config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ class Rkn_Botz(object):
     PORT = int(os.environ.get("PORT", "8080"))
 
     # Force subscribe channel username (without @) (only public chats username required)
-    FORCE_SUB = os.environ.get("FORCE_SUB", "")
+    FORCE_SUB = os.environ.get("FORCE_SUB", "Luxebotupdate")
 
     # Database config (required)
     DB_NAME = os.environ.get("DB_NAME", "AutoCaption_V05_Bot")
@@ -66,6 +66,7 @@ class Rkn_Botz(object):
 
 
 # ⚠️ Please do not remove this credit!
+
 
 
 
